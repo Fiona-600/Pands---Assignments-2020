5f142e40b97b75cd2d4210a84694030ee6254a51
Fiona <G00387835@gmit.ie>
2020-04-03 22:02:10 +0100

    Format Wk6-Squareroot-Using_Sqrt.py

--- Wk6-Squareroot-Using_Sqrt.py
+++ Wk6-Squareroot-Using_Sqrt.py
@@ -2,27 +2,17 @@
 # A function to find the square root of a given number
 
 import math 
-
 a = float ( input ("Please enter a positive number: "))
 
 while True:
-
     if a > 0:
-   
         b = math.sqrt (a)
-
         print ("The square root of", a ,"is approx.", round ( b , 1))
-               
     else:
-              
         if a < 0:
         #If user tries to input a negative number in error
-
             print ("Error!! Number must be positive...please try again")
-
             a = float ( input ("Please enter a positive number: "))
-            
             continue
-    
     break
 
