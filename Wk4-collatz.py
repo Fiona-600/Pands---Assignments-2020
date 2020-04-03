b3301dd69ecb8140d6ae2ba33b637d742f5c2321
Fiona <G00387835@gmit.ie>
2020-04-03 20:20:03 +0100

    Updated Wk4-Collatz.py

--- Wk4-collatz.py
+++ Wk4-collatz.py
@@ -4,34 +4,27 @@
 #At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
 #Have the program end if the current value is one
 
-#a = Current Value
 print()
 a = int(input("Please Enter any Positive Integer: "))
 print()
 print ("Current Value is:",int(a))
+#a = Current Value
 print()
 
 #Over-riding condition to execute only until a equals one
-
 while a != 1:
-
-      #Condition to be satisfied if Current Value is EVEN:
-
+    
+    #Condition to be satisfied if Current Value is EVEN:
     if(a % 2) == 0:
         a / 2
         print("Current value is EVEN so successive value is",a,"divided by 2 = ",int(a / 2))
-
-
     else:
-
         #Condition to be satisfied if Current Value is ODD:
-
         if(a % 2) != 0:
             ((a * 3) + 1)
             print("Current value is ODD so successive value is",a,"multiplied by 3 plus one = ",int((a * 3) + 1))
 
     #Loop condition to create successive values:
-
     if (a % 2) == 0: 
         a = int(a / 2)
     else:
