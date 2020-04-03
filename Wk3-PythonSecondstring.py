f9b226b5502245086eefd67625fa045e93bd6163
Fiona <G00387835@gmit.ie>
2020-04-03 20:22:55 +0100

    Amendment to Wk3-PythonSecondstring.py

--- Wk3-PythonSecondstring.py
+++ Wk3-PythonSecondstring.py
@@ -4,7 +4,6 @@
 Sentence = input("Please Enter a Sentence:  ")
 #User Input = "The quick brown fox jumps over the lazy dog..o zletrv pu o wr cu h"
 #Output is every second letter of user input in reverse order
-
 print ()
 print("Your sentence in reverse order showing every second letter is: ",Sentence[:0:-2])
 #This also works with print(Sentence[::-2])
