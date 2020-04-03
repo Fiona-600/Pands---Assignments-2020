57b0b4ee6c291ce413bd82d0cd0fe01e934a1d6a
Fiona <G00387835@gmit.ie>
2020-04-03 22:08:53 +0100

    Format file Wk7-Es.py

--- Wk7-Es.py
+++ Wk7-Es.py
@@ -6,11 +6,9 @@
 
 with open ("Moby-Dick.txt" , "r") as f:
 # Open text file called 'Moby-Dick.text file in read mode
-   
    print("")
    frequency = (f.read () .lower (). count ('e'))
    # Read all characters in the file, convert to lower case and count 
    # the number of times 'e' appears
-
    print ("The number of times 'e' occurs in the book 'Moby Dick' is:" , frequency)
    # Print the result
