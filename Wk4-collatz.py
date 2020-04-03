0f52ef71dd59db5d4a0ba8776d2983f4a4e7d868
Fiona <G00387835@gmit.ie>
2020-04-03 21:47:23 +0100

    Change value of variable from 'a' to 'value'

--- Wk4-collatz.py
+++ Wk4-collatz.py
@@ -1,32 +1,29 @@
 #Fiona Lee
-
 #Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation
 #At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
 #Have the program end if the current value is one
 
 print()
-a = int(input("Please Enter any Positive Integer: "))
+value = int (input ("Please Enter any Positive Integer: "))
 print()
-print ("Current Value is:",int(a))
-#a = Current Value
+print ("Current Value is:", int (value))
+#value = Current Value
 print()
-
 #Over-riding condition to execute only until a equals one
-while a != 1:
-    
+while value != 1:
     #Condition to be satisfied if Current Value is EVEN:
-    if(a % 2) == 0:
-        a / 2
-        print("Current value is EVEN so successive value is",a,"divided by 2 = ",int(a / 2))
+    if(value % 2) == 0:
+        value / 2
+        print("Current value is EVEN so successive value is", value ,"divided by 2 = ",int(value / 2))
     else:
         #Condition to be satisfied if Current Value is ODD:
-        if(a % 2) != 0:
-            ((a * 3) + 1)
-            print("Current value is ODD so successive value is",a,"multiplied by 3 plus one = ",int((a * 3) + 1))
+        if(value % 2) != 0:
+            ((value * 3) + 1)
+            print("Current value is ODD so successive value is", value ,"multiplied by 3 plus one = ",int((value * 3) + 1))
 
     #Loop condition to create successive values:
-    if (a % 2) == 0: 
-        a = int(a / 2)
+    if (value % 2) == 0: 
+        value = int(value / 2)
     else:
-        a = int((a * 3) + 1)
+        value = int((value * 3) + 1)
 
