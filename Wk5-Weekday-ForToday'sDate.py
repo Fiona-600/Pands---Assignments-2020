5ba41e7348616b99dfc9011ab3c790b73c12e6b1
Fiona <G00387835@gmit.ie>
2020-04-03 21:10:34 +0100

    Amended Version of Wk5-Weekday-ForToday'sDate.py

--- Wk5-Weekday-ForToday'sDate.py
+++ Wk5-Weekday-ForToday'sDate.py
@@ -1,23 +1,24 @@
 # Fiona Lee
-
 # Write a program that outputs whether or not today is a weekday
+
+# References: https://www.codespeedy.com/find-the-day-of-week-with-a-given-date-in-python/
+# References: https://stackoverflow.com/questions/311627/how-to-print-a-date-in-a-regular-format
 
 import datetime
 
 Week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
 # Defines weekdays as a list
 
-now = datetime.datetime.now()
-# Returns date in year, month day, hour, min, second, milisecond
+today = datetime.datetime.today()
+# Returns today's date in year, month, day & time
 
-day = now.weekday()
+day = today.weekday()
 # Returns position of the day of the week in number format 0-6
 
 if day < 5:
-
+   print()
    print("Today is", Week_days[day],"... Yes, Unfortunately today is a weekday.")
    #show day of the week with reference to position defined by 'day' in 'Week_days' list
 
 else:
-    
    print("Today is", Week_days[day],"... 'It's the weekend, yay!")
